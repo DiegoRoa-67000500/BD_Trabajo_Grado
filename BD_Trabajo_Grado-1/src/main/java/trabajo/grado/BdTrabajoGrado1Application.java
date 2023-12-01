9dde11bf4bd3e5d9a9d40602e4263ec9757e1fb9
Diego Roa <diego.roa.lepeley@gmail.com>
2023-12-01 16:59:58 -0500

    Inicio de la BD

---
+++ BD_Trabajo_Grado-1/src/main/java/trabajo/grado/BdTrabajoGrado1Application.java
@@ -0,0 +1,13 @@
+package trabajo.grado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdTrabajoGrado1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdTrabajoGrado1Application.class, args);
+	}
+
+}
